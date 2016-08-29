ae47fd282212a14eac64c274e068fa42f388d207
kazuyanakamura <kazuyan753@gmail.com>
2016-08-29 18:03:39 -0300

    Organização das bibliotecas modificadas

---
+++ robo_sensors/robo_sensors.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *
+ * INCLUDES
+ *
+ ******************************************************************************/
+#include <Arduino.h>
+#include "./robo_sensors.h"
+#include "../../robo_mov/src/robo_mov.h"
+
+/*******************************************************************************
+ *
+ * FUNCTIONS
+ *
+ ******************************************************************************/
+
+void SENSORS_Start(void){
+  pinMode(IR_PIN, INPUT);
+}
+
+// Pega os valores lidos por cada porta e atribui os valores aos
+// devidos ponteiros
+void SENSORS_CheckAll(sensors_t* get_value){
+  get_value->line_fr = analogRead(LINE_FR_PIN);
+  get_value->line_fl = analogRead(LINE_FL_PIN);
+  get_value->line_br = analogRead(LINE_BR_PIN);
+  get_value->line_bl = analogRead(LINE_BL_PIN);
+  get_value->ir = digitalRead(IR_PIN);
+}
+
+// Importante chamar essa funcao no comeco do codigo para ter uma ideia da
+// do sensor e calibrar qual a tolerancia maxima (sujeira pode interferir na
+// leitura), para evitar leituras erradas
+void SENSORS_CalibrateLineSensor(int* min, int* max){
+  min = analogRead(LINE_FR_PIN);
+  max = min * 2;
+}
